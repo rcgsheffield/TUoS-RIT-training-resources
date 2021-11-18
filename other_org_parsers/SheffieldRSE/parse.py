0348ee5c50ed14f5ea7a74853a5b457a94df20ec
James Moore <j.k.moore@sheffield.ac.uk>
2021-11-18 17:11:53 +0000

    Add content level and topic matching / parsing.

--- other_org_parsers/SheffieldRSE/parse.py
+++ other_org_parsers/SheffieldRSE/parse.py
@@ -14,26 +14,37 @@ def md_to_text(md):
     soup = BeautifulSoup(html, features='html.parser')
     return soup.get_text()
 
+
+
 if platform == "linux" or platform == "linux2":
 
-    current_path = (os.path.dirname(__file__))
+    Core_Matches = ["starter", "beginner", "easy"]
+    Intermediate_Matches = ["middle", "average", "medium"]
+    Advanced_Matches = ["end", "difficult", "hard"]
+    ML_Matches = ["deep learning","machine learning","deep_learning","deep-learning","machine_learning","machine-learning"]
+    Software_Carpentry_Matches = ["software data carpentry"]
+
+    current_path = (os.path.dirname(os.path.realpath(__file__)))
 
     #Bash
     # Download the URL then strip to the folder you want.
     #https://codeload.github.com/[owner]/[repo]/tar.gz/master | \ tar -xz --strip=2 [repo]-master/[folder_path]
-    # Uncomment and use or download the markdown manually into a folder called events in the same directory as this script.
+    # Un/recomment and use or download the markdown manually into a folder called events in the same directory as this script.
 
-    """
+    #"""
     owner = "RSE-Sheffield"
     repo =  "RSE-Sheffield.github.io"
-    shutil.rmtree(current_path + "/events", ignore_errors=True)
-    os.makedirs(os.path.dirname(current_path + "/events"))
+    print(current_path)
+    events_path = current_path + "/events"
+    print(events_path)
+    shutil.rmtree(events_path , ignore_errors=True)
+    os.makedirs(events_path)
     curl = " curl https://codeload.github.com/" + owner + "/" + repo +"/tar.gz/master"
     untarcmd = " | tar -C events -xz --strip=2 " + repo + "-master/_events"
 
     bashCommand = curl + untarcmd
     output = subprocess.check_output(['bash','-c', bashCommand])
-    """
+    #"""
 
     out_folder = "parsed_files" 
     shutil.rmtree(current_path + "/" + out_folder, ignore_errors=True)
@@ -54,7 +65,12 @@ if platform == "linux" or platform == "linux2":
                                                                                                                     # The new lines are removed when the full link is constructed below in line 67.
                 if contentlevel_regex:
                     contentlevel = contentlevel_regex[0]
-                    # Further mapping of difficulty level is needed here.
+                    if any (x in contentlevel_regex[0] for x in Core_Matches):
+                        contentlevel = "Core"
+                    elif any (x in contentlevel_regex[0] for x in Intermediate_Matches):
+                        contentlevel = "Intermediate"
+                    elif any (x in contentlevel_regex[0] for x in Advanced_Matches):
+                        contentlevel = "Advanced"
                 else:
                     contentlevel = "unknown"
                 
@@ -62,12 +78,25 @@ if platform == "linux" or platform == "linux2":
                     permalink = "https://rse.shef.ac.uk" + permalink_regex                                          # The RSE website is hosted on this alternate DNS name.
                 else:
                     permalink = ""
+
+                topic_list = []
+                if tags:
+                    if any (x in tags for x in ML_Matches):
+                        topic_list.append("ML")
+                    elif any (x in tags for x in Software_Carpentry_Matches):
+                        topic_list.append("SC")
+                    topic = ",".join(topic_list)
+                else:
+                    topic = "unknown"
                 
                 #Begin full link construction into YAML.
-                link = "- title: \"" + title + "\"\n" + "  description: \" " + md.replace("\n", "") + "\"\n" + "  pageName: training.md" + "\n" + "  level: " + contentlevel + "\n" + "  topic: " + "insertmapper" + "\n" + "  provider: Sheffield-RSE" + "\n" + "  tags: " + tags + "\n"
+                link = "- title: \"" + title + "\"\n" + "  description: \" " + md.replace("\n", "") + "\"\n" + "  pageName: training.md" + "\n" + "  level: " + contentlevel + "\n" + "  topic: " + topic + "\n" + "  provider: Sheffield-RSE" + "\n"
+                if tags:
+                    link = link + "  tags: " + tags + "\n"
+                if date:
+                    link = link + "  date: " + date + "\n"
                 if permalink:
                     link = link + "  url: " + permalink + "\n"
-                #print (link)
                 link = link + "\n"
                 #Finish full link construction into YAML.
 
